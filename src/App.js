46e26082f974b6424f4ccc849f74804b2fdbdc8c
Muhammed Musthafa <musthafak49@gmail.com>
2022-11-10 13:50:06 +0530

    Advices API added...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from "react";
 import "./App.css";
-
+import axios from "axios";
 
 
 class App extends React.Component {
-  
-  render() {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount(){
+    this.fetchAdvice();
     
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const {advice} =response.data.slip;
+        this.setState({advice});
+      })
+
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+ 
+  render() {
+    const {advice}= this.state;
     return( 
-      <h1>APP</h1>
+      <div className="app">
+          <div className="card">
+            <h1 className="heading">{advice}</h1>
+          </div>
+      </div>
     );
   }
 }
